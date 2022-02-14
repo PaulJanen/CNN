9227a7badcb4d1d94ecbf70598a576d815c4ca68
Kazkas niekas <murkuzas1@gmail.com>
2022-02-14 19:43:17 +0200

    Convolution Layer

--- test/codeTest.py
+++ test/codeTest.py
@@ -8,5 +8,12 @@ print(a_prev.shape)
 np.sum(a_prev, axis=(2,3))
 
 
-
-
+a = np.array([[ 1.,  1.,  1.,  1.,  1.], [ 1.,  1.,  1.,  1.,  1.], [ 1.,  1.,  1.,  1.,  1.]])
+a
+np.pad(a, [(0, 0), (0,0)], mode='constant', constant_values=0)
+np.pad(a, [(1, 0), (0,0)], mode='constant', constant_values=0)
+np.pad(a, [(0, 1), (0,0)], mode='constant', constant_values=0)
+np.pad(a, [(1, 1), (0,0)], mode='constant', constant_values=0)
+np.pad(a, [(1, 1), (1,0)], mode='constant', constant_values=0)
+np.pad(a, [(1, 1), (0,1)], mode='constant', constant_values=0)
+np.pad(a, [(1, 1), (1,1)], mode='constant', constant_values=0)
